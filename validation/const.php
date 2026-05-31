38c8ed17558155e3dc31277d6f42f5a839f32e2f
San <sanwebdev@yandex.ru>
2026-05-31 20:33:17 +0300

    Remove double const

--- validation/const.php
+++ validation/const.php
@@ -5,4 +5,3 @@ declare(strict_types=1);
 const VALIDATOR_SEPARATOR = ':';
 const VALIDATOR_PARAMS_SEPARATOR = '&';
 const VALIDATOR_PARAM_VALUE_SEPARATOR = '=';
-const VALIDATOR_FUNCTION_PREFIX = 'validate_';
