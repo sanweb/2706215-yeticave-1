a3330b8f10aa3d450dec2fac4b6427347e899bbf
San <sanwebdev@yandex.ru>
2026-06-03 11:51:21 +0300

    Move validation constants to the only script that uses them

--- validation/main.php
+++ validation/main.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+const VALIDATOR_SEPARATOR = ':';
+const VALIDATOR_PARAMS_SEPARATOR = '&';
+const VALIDATOR_PARAM_VALUE_SEPARATOR = '=';
+
 /**
  * Validates required form fields and returns validation errors.
  *
